dc4a3b350063229ac405f2dfc4530c7bf2a8360d
Jérôme_Préauchat <jerome.preauchat@gmail.com>
2020-01-21 21:11:55 +0100

    Update GD1_2_PREAUCHAT_JEROME_TP1_Systeme_de_combat.c

--- GD1_2_PREAUCHAT_JEROME_TP1_Systeme_de_combat.c
+++ GD1_2_PREAUCHAT_JEROME_TP1_Systeme_de_combat.c
@@ -4,99 +4,99 @@
 
 
 int main (){
-	int pvmonstre = 100;
-	int attaquejoueur = 10;
-	int pvjoueur = 100;
-	int attaquemonstre = 10;
+    int pointsDeVieJoueur = 100;
+	int pointsDeVieMonstre = 100;
+	int attaqueJoueur = 10;
+	int attaqueMonstre = 10;
 	int contreAttaque = 0;
-	int decisionJ = 0;
-	int decisionM = 0;
-	int PM = 10;
-	int PMmonstre = 10;
-	int poisonJ = 0;
-	int poisonM = 0;
+	int decisionJoueur = 0;
+	int decisionMonstre = 0;
+	int PointsDeManaJoueur = 10;
+	int PointsDeManaMonstre = 10;
+	int poisonJoueur = 0;
+	int poisonMonstre = 0;
 	int antidote = 0;
 	
 	srand(time(NULL));
 
-	while(pvmonstre>0 && pvjoueur>0){
-		printf ("Voulez vous attaquer (1) ou vous vous defendre (2) ou vous decidez de l'empoisonner (3) ou d'utiliser l'antidote (4)?\n");
-		scanf("%d",&decisionJ);
-		decisionM =((rand()%3)+1);
+	while(pointsDeVieMonstre>0 && pointsDeVieJoueur>0){
+		printf ("Voulez vous attaquer (1), vous defendre (2) ou empoisonner le monstre (3) ou bien utiliser l'antidote (4) ? \n");
+		scanf("%d",&decisionJoueur);
+		decisionMonstre =((rand()%3)+1);
 		
-		if (decisionJ == 4){
+		if (decisionJoueur == 4){
 			printf("Vous utilisez votre antidote, vous n'etes plus empoisonne");
-			poisonM = 0;
+			poisonMonstre = 0;
 		}
 			
-		if (decisionJ == 3 && PM>=3){
-			printf("Vous avez empoisonne le monstre.\n");
-			poisonJ ++;
-			PM = PM -3;
-			if(decisionJ == 3 && PM<3){
-				printf ("Vous n'avez plus de mana\n");
-				printf ("Voulez vous attaquer (1) ou vous vous defendre (2)\n");
-				scanf("%d",&decisionJ);
+		if (decisionJoueur == 3 && PointsDeManaJoueur>=3){
+			printf("Vous empoisonnez le monstre.\n");
+			poisonJoueur ++;
+			PointsDeManaJoueur = PointsDeManaJoueur -3;
+			if(decisionJoueur == 3 && PointsDeManaJoueur<3){
+				printf ("Vous n'avez plus de mana et vous ne pouvez empoisonner le monstre. \n");
+				printf ("Voulez vous attaquer (1) ou vous vous defendre (2) .\n");
+				scanf("%d",&decisionJoueur);
 			}
 		}
 		
 		
-		if (decisionM == 3 && PMmonstre>=3){
-			printf("Le monstre vous empoisonne.\n");
-			poisonM ++;
-			PMmonstre = PMmonstre -3;
-			if(decisionM == 3 && PMmonstre<3){
-				(decisionM =((rand()%2)+1));
+		if (decisionMonstre == 3 && PointsDeManaMonstre>=3){
+			printf("Le monstre vous empoisonne. \n");
+			poisonMonstre ++;
+			PointsDeManaMonstre = PointsDeManaMonstre -3;
+			if(decisionMonstre == 3 && PointsDeManaMonstre<3){
+				(decisionMonstre =((rand()%2)+1));
 			}
 		}
 		
 			
-		if (decisionM == 2){
+		if (decisionMonstre == 2){
 			printf("Le monstre se defend.\n");
-			attaquejoueur = attaquejoueur/4;
+			attaqueJoueur = attaqueJoueur/4;
 				
 		}
 		
-		if (decisionJ == 2){
-			printf("Vous decidez de vous defendre.\n");
-			attaquemonstre = attaquemonstre/4;
+		if (decisionJoueur == 2){
+			printf("Vous avez decide de vous defendre.\n");
+			attaqueMonstre = attaqueMonstre/4;
 			
 		}
 		
-		if(decisionJ == 1){
-			printf("Vous decidez de l'attaquer.\n");
-			pvmonstre = pvmonstre - attaquejoueur;
+		if(decisionJoueur == 1){
+			printf("Vous decidez de l'attaquer. \n");
+			pointsDeVieMonstre = pointsDeVieMonstre - attaqueJoueur;
 			
 			}
 		
-		if(decisionM == 1){
-			printf("Le monstre vous attaque.\n");
-			pvjoueur = pvjoueur - attaquemonstre;
+		if(decisionMonstre == 1){
+			printf("Le monstre vous attaque. \n");
+			pointsDeVieJoueur = pointsDeVieJoueur - attaqueMonstre;
 			
 		}
 	
-	if(poisonJ >= 1){
-		pvmonstre = pvmonstre - poisonJ;
+	if(poisonJoueur >= 1){
+		pointsDeVieMonstre = pointsDeVieMonstre - poisonJoueur;
 	}
-	if(poisonM >= 1){
-		pvjoueur = pvjoueur - poisonM;
+	if(poisonMonstre >= 1){
+		pointsDeVieJoueur = pointsDeVieJoueur - poisonMonstre;
 	}
 		
 		
-	if (PM<10){
-		PM++;
+	if (PointsDeManaJoueur<10){
+		PointsDeManaJoueur++;
 	}
 	
-	if (PMmonstre<10){
-		PMmonstre++;
+	if (PointsDeManaMonstre<10){
+		PointsDeManaMonstre++;
 	}
 	
-	printf ("Il vous reste %d points de mana.\n", PM);
-	printf ("Le monstre a %d points de vie.\n", pvmonstre);
-	printf ("Il vous reste %d points de vie.\n", pvjoueur);
+	printf ("Il vous reste %d points de mana.\n", PointsDeManaJoueur);
+	printf ("Le monstre a %d points de vie.\n", pointsDeVieMonstre);
+	printf ("Il vous reste %d points de vie.\n", pointsDeVieJoueur);
 	
-	attaquemonstre = 10;
-	attaquejoueur = 10;
+	attaqueMonstre = 11;
+	attaqueJoueur = 10;
 	
 		
 	}	
